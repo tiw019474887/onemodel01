e81bd9dddd865bc1168421e5d75d85af3045a991
tiw019474887 <tiw_t1@hotmail.com>
2015-02-16 23:38:35 +0700

    เพิ่ม เลาเตอร์ หน้าView

--- app/routes.php
+++ app/routes.php
@@ -17,4 +17,7 @@ Route::get('show', function()
 });
 Route::controller('show/project','ProjectController');
 Route::controller('show/faculty','FacultyController');
+Route::controller('show/faculty/view','FacultyViewController');
+Route::controller('show/project/view','ProjectViewController');
+
 
